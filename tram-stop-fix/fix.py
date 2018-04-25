b3d357627754da80a7c8b8d85acffd2e54b15c2f
oybek <aybek_hashimov@mail.ru>
2018-04-25 22:56:16 +0500

    get troll stops

--- tram-stop-fix/fix.py
+++ tram-stop-fix/fix.py
@@ -6,16 +6,16 @@ import urllib.request
 from pprint import pprint
 
 url = 'https://m.ettu.ru/station/'
-data = json.load(open('tram-stops.json', encoding='UTF-8'))
+data = json.load(open('troll-stops.json', encoding='UTF-8'))
 
 for element in data:
     req = url + element['ID']
     try: 
         urllib.request.urlopen(req)
     except urllib.error.HTTPError as e:
-        print('')
+        pass
     except urllib.error.URLError as e:
-        print('')
+        pass
     else:
         print(',', element)
 
