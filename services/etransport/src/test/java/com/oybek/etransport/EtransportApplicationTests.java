11763a2cde2762e4b514f828ade29562ccf29dc4
oybek <aybek.hashimov@gmail.com>
2018-11-11 21:08:07 +0500

    etransport service reborn start

---
+++ services/etransport/src/test/java/com/oybek/etransport/EtransportApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oybek.etransport;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EtransportApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
